d97276cab42588c6913b31396a43aafa2918f779
kuzuzo <llewelyncos@gmail.com>
2025-11-17 20:01:15 +0000

    2 a-c done

--- src/euclidean.ads
+++ src/euclidean.ads
@@ -4,6 +4,9 @@ package Euclidean with SPARK_Mode is
 
    procedure Euclidean_Algorithm(a : in Natural;
                                  b : in Natural;
-                                 gcd : out Natural);
+                                 gcd : out Natural)
+     with
+       Global => null,
+       Depends => (gcd => (A,B));
 
 end Euclidean;
--- src/main.adb
+++ src/main.adb
@@ -1,43 +1,52 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Euclidean; use Euclidean;
+Pragma SPARK_Mode (On);
 
+with AS_Io_Wrapper; use AS_Io_Wrapper;
+with Euclidean; use Euclidean;
 
 procedure Main is
 
-   A, B, G : Integer;
-   Continue : Character := 'Y';
 
-   begin
-   while Continue = 'Y' or Continue = 'y' loop
+   A, B, G : Integer;
+   User_Input : String(1..20);
+   Last : Integer;
+
+begin
+   AS_Init_Standard_Input;
+   AS_Init_Standard_Output;
+
+   loop
       -- Input first number
       loop
-         Put("enter the a value: ");
-         Get(A);
+         AS_Put("enter the a value: ");
+         AS_Get(A);
          if A > 0 then
             exit;
          else
-            Put_Line("number not greater than 0");
+            AS_Put_Line("number not greater than 0");
          end if;
       end loop;
 
       -- Input second number
       loop
-         Put("enter the b value: ");
-         Get(B);
+         AS_Put("enter the b value: ");
+         AS_Get(B);
          if B > 0 then
             exit;
          else
-            Put_Line("number not greater than 0");
+            AS_Put_Line("number not greater than 0");
          end if;
       end loop;
 
       Euclidean.Euclidean_Algorithm(A, B, G);
-      Put_Line("gcd is: " & Natural'Image(G));
+      AS_Put_Line("gcd is: " & Natural'Image(G));
 
-      -- Ask whether to continue
-      Put("Do you want to continue? (Y/N): ");
-      Get(Continue);
+         loop
+         As_Put("Do you want to try again (y/n)? ");
+         As_Get_Line(User_Input, Last);
+         exit when Last > 0;
+         As_Put_Line("Please enter a non-empty string");
+      end loop;
+      exit when User_Input(1 .. 1) = "n";
    end loop;
 
 end Main;
