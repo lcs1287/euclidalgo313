6adf8e8431750e5fd8d5834ca42d7b99a1be571c
kuzuzo <llewelyncos@gmail.com>
2025-11-17 18:28:58 +0000

    lol

--- src/euclidean.ads
+++ src/euclidean.ads
@@ -1,1 +1,7 @@
 Pragma SPARK_Mode (On);
+
+package Euclidean with SPARK_Mode is
+
+   procedure Euclidean_Algorithm(a : in Natural;
+                                 b : in Natural;
+                                 gcd : out Natural);
